b8cb360f557e5af3ea3dca73efb1b30326213ccb
Alex Sweeney <56929496+teaquest@users.noreply.github.com>
2026-03-16 19:01:48 -0400

    simple calculator app start. Will change the file name later

---
+++ test.py
@@ -0,0 +1,30 @@
+def add (number1, number2):
+    return number1 + number2
+
+def subtract (number1, number2):
+    return number1 - number2
+
+def multiply (number1, number2):
+    return number1 * number2
+
+def divide (number1, number2):
+    return number1 / number2
+
+num1 = 2
+num2 = 2
+
+#print('1')
+
+print(num1)
+print(num2)
+
+add_result = add(num1, num2)
+print('Addition: ', add_result)
+
+print('Subtraction: ', subtract(num2, num1))
+
+print('Multiplication: ', multiply(num2, num1))
+
+print('Division: ', divide(num2, num1))
+
+#TODO: Add a while, for, and dowhile loop to this
